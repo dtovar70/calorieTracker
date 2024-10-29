c6b299ba6751cc079d279dcb7712962e536b49f1
dtovar <jtovar.ks2.tech>
2024-10-28 21:03:19 -0400

    Agregado cambios en el proyecto

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,70 @@
+import { useState, ChangeEvent, FormEvent, Dispatch, useEffect } from "react"
+import {v4 as uuidv4} from 'uuid'
+import { Activity } from "../types"
 import { categories } from "../data/categories"
+import { ActivityActions, ActivityState } from "../reducers/activity-reducer"
 
-export default function Form() {
+type FormProps = {
+    dispatch: Dispatch<ActivityActions>,
+    state: ActivityState
+}
+
+const initialState: Activity = {
+    id: uuidv4(),
+    category: 1,
+    name: '',
+    calories: 0
+}
+
+export default function Form({dispatch, state}: FormProps) {
+
+    const [activity, setActivity] = useState<Activity>(initialState)
+
+    useEffect(() => {
+        if(state.activeId){
+            const selectActivity = state.activities.filter( stateActivity => stateActivity.id === state.activeId)[0];
+            setActivity(selectActivity);
+        }
+    }, [state.activeId])
+
+    const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+
+        const isNumberField = ['category', 'calories'].includes(e.target.id);
+        
+        setActivity({
+            ...activity,
+            [e.target.id]: isNumberField ? +e.target.value : e.target.value
+        })
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        dispatch({type: 'save-activity', payload: {newActivity: activity}});
+        setActivity({
+            ...initialState,
+            id: uuidv4()
+        });
+    }
+
+    const isValidActivity = () => {
+        const { name, calories } = activity;
+        return name.trim() !== '' && calories > 0;
+    }
+
   return (
     <div>
-      <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+      <form 
+        className="space-y-5 bg-white shadow p-10 rounded-lg"
+        onSubmit={handleSubmit}
+      >
         <div className="grid grid-cols-1 gap-3">
             <label htmlFor="category" className="font-bold">Categoría</label>
             <select className="border border-slate-300 p-2 rounded-lg w-full bg-white"
-             id="category">
+             id="category"
+             value={activity.category}
+             onChange={handleChange}
+             >
                 {categories.map(category => (
                     <option key={category.id} value={category.id}>
                         {category.name}
@@ -17,11 +74,13 @@ export default function Form() {
         </div>
 
         <div className="grid grid-cols-1 gap-3">
-            <label htmlFor="activity" className="font-bold">Actividad</label>
+            <label htmlFor="name" className="font-bold">Actividad</label>
             <input type="text" 
-                id="activity"
+                id="name"
                 className="border border-slate-300 p-2 rounded-lg"
                 placeholder="Ej. Comida, jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+                value={activity.name}
+                onChange={handleChange}
             />    
         </div>
 
@@ -30,14 +89,18 @@ export default function Form() {
             <input type="number" 
                 id="calories"
                 className="border border-slate-300 p-2 rounded-lg"
-                placeholder="Calorías Ej. 300, 500 "
+                placeholder="Calorías Ej. 300, 500"
+                value={activity.calories}
+                onChange={handleChange}
             />    
         </div>
 
         <input 
             type="submit" 
-            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
-            value='Guardar comida o guardar ejercicio'
+            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer
+            disabled:opacity-50"
+            value={activity.category === 1 ? 'Guardar Comida' : 'Guardar Ejercicio'}
+            disabled={!isValidActivity()}
         />
       </form>
     </div>
